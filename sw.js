4b55ac32a197abf9a2bc02479aa2ff4c0bbf18af
RMBGold-code <rocketshielder66@gmail.com>
2026-04-04 16:51:12 -0400

    Revert "Bundle Leaflet locally for map boot"
    This reverts commit bde6fb53e4c32fd8a7bdb2d47e8e67dd3d12923a.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "howard-landmarks-v60";
+const CACHE_NAME = "howard-landmarks-v59";
 const APP_SHELL = [
   ".",
   "index.html",
@@ -7,8 +7,6 @@ const APP_SHELL = [
   "app-core.js",
   "app-ui.js",
   "manifest.json",
-  "vendor/leaflet.css",
-  "vendor/leaflet.js",
   "icons/icon.svg",
   "icons/install-qr.svg"
 ];
